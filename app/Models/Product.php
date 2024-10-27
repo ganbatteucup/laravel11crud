a5d4b21bdf036ce7b0d3fcabf8635daeb5d66deb
ganbatteucup <2011501025@student.unisayogya.ac.id>
2024-10-27 15:27:12 +0700

    membuat model dan migration ke database

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory; // Mengimpor trait HasFactory untuk mendukung factory pada model ini
+
+    // Menentukan atribut mana saja yang boleh diisi secara massal
+    protected $fillable = [
+        'image',        // Menyimpan URL atau path gambar item
+        'title',        // Menyimpan judul atau nama item
+        'description',  // Menyimpan deskripsi detail item
+        'price',        // Menyimpan harga item
+        'stock',        // Menyimpan jumlah stok item yang tersedia
+    ];
+    
+}
